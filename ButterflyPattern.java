b2fc21e8a08d351aee71e78ee8adf43b4258f435
ANILKUMAR00004 <gaddamaduguanilkumar@gmail.com>
2025-08-14 10:42:41 +0530

    Update and rename Main.java to ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ButterflyPattern {
     public static void main(String[] args) {
         int k=4;
         for(int i=1;i<k;i++){
